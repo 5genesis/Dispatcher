4b0b3c810d1abac235ef87561d50072b0ceb8351
LuisGA-UPM <36197267+LuisGA-UPM@users.noreply.github.com>
2020-12-09 10:50:17 +0100

    Update distributor.py

--- distributor/distributor.py
+++ distributor/distributor.py
@@ -96,7 +96,7 @@ def proxy(path):
         elif request.method == 'DELETE':
             resp = requests.delete(f'{SITE_NAME}{path}')
 
-        logger.info(str(resp.text))
+        # logger.info(str(resp.text))
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         # headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         # response = Response(resp.content, resp.status_code) #, headers)
